3778211980f4815a84523a24fc2622c5a6b62ada
HansolWorld <hs9269@gmail.com>
2023-03-29 21:02:39 +0900

    link ContentView from IntroView

--- MC1-Team10-Dum.swiftpm/Sources/Sol&Cindy/IntroView.swift
+++ MC1-Team10-Dum.swiftpm/Sources/Sol&Cindy/IntroView.swift
@@ -9,21 +9,23 @@ import SwiftUI
 
 struct IntroView: View {
     
-    let backgroundEffectOffsetY: [CGFloat] = [100, 0, 100, 100, 100, 100]
-    let planetFrameWidth: [CGFloat] = [400, 500, 1000, 1000, 1000, 1000]
-    let planetOffsetY: [CGFloat] = [-100, 15, 500, 500, 500, 500]
+    let backgroundEffectOffsetY: [CGFloat] = [100, 0, 100, 100, 100, 100, 100]
+    let planetFrameWidth: [CGFloat] = [400, 500, 1000, 1000, 1000, 1000, 1000]
+    let planetOffsetY: [CGFloat] = [-100, 15, 500, 500, 500, 500, 500]
     
-    let titleFirstString: [CGFloat] = [-500, 0, 0, 0, 0, 0]
-    let titleSecondString: [CGFloat] = [-200, 0, 0, 0, 0, 0]
-    let titleThirdString: [CGFloat] = [-80, 0, 0, 0, 0, 0]
-    let titleFourthString: [CGFloat] = [-170, 0, 0, 0, 0, 0]
+    let titleFirstString: [CGFloat] = [-500, 0, 0, 0, 0, 0, 0]
+    let titleSecondString: [CGFloat] = [-200, 0, 0, 0, 0, 0, 0]
+    let titleThirdString: [CGFloat] = [-80, 0, 0, 0, 0, 0, 0]
+    let titleFourthString: [CGFloat] = [-170, 0, 0, 0, 0, 0, 0]
 
-    private var buttonText: [String] = ["", "- start -", "- Next -", "- Next -", "- Next -", "- Next -"]
-    private var printText: [String] = ["", "", "3029년, 황금의 행성 '덤도라도'", "이곳에 엄청난 개발자로 성장할 수 있는 '덤덤물약'이 있다고 하는데...", "", "덤나라 용사가 최고의 개발자로 성장하기 위해 물약을 찾으러 모험을 떠난다!!"]
+    private var buttonText: [String] = ["", "- start -", "- Next -", "- Next -", "- Next -", "- Next -", ""]
+    private var printText: [String] = ["", "", "3029년, 황금의 행성 '덤도라도'", "이곳에 엄청난 개발자로 성장할 수 있는 '덤덤물약'이 있다고 하는데...", "", "덤나라 용사가 최고의 개발자로 성장하기 위해 물약을 찾으러 모험을 떠난다!!", ""]
     
     @State private var pageIndex: Int = 0
     @State private var startFlag: Bool = false
+    @State private var endFlag: Bool = false
     @State private var textFlag: Bool = false
+    
     var body: some View {
         ZStack {
             Image("intro_background")
@@ -73,12 +75,21 @@ struct IntroView: View {
                     if startFlag==false {
                         startFlag = true
                     }
+                    if pageIndex > 5 {
+                        endFlag = true
+                    }
                 }) {
                     Text("\(buttonText[pageIndex])")
                         .foregroundColor(.white)
                         .bold()
                         .font(.system(size: 30))
                         .animation(.linear(duration: 2), value: pageIndex)
+                        .isHidden(endFlag, remove: endFlag)
+                    NavigationLink(destination: ContentView()) {
+                        Text("게임시작!")
+                    }
+                    .isHidden(!endFlag)
+                    
                 }
             }
             .offset(y:300)
