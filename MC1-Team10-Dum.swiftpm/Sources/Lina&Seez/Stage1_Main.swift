324680e7efd043839e21ceb9ac15eae0c501105d
seunghoria <seungeun991114@gmail.com>
2023-03-29 16:08:28 +0900

    feat: 잎 드래그 제거 기능 구현

--- MC1-Team10-Dum.swiftpm/Sources/Lina&Seez/Stage1_Main.swift
+++ MC1-Team10-Dum.swiftpm/Sources/Lina&Seez/Stage1_Main.swift
@@ -7,93 +7,55 @@
 
 import SwiftUI
 
-
-import SwiftUI
-
 struct Stage1_Main: View {
+    var removal: (() -> Void)? = nil
     
-    @State private var offset: CGSize = .zero
-    
-    
-    var body: some View {
         
-        let drag = DragGesture()
-                    .onChanged { gesture in
-                        offset=gesture.translation}
-                    .onEnded {
-                        //드래그 가로의 위치가 -100보다 작은 위치로 가면 실행
-                        if $0.translation.width < -100 {
-                            self.offset = .init(width: -1000, height: 0)
-                        //드래그 가로의 위치가 100보다 커지면 실행
-                        } else if $0.translation.width > 100 {
-                            self.offset = .init(width: 1000, height: 0)
-                        //아니면 원래 위치로 돌아감
-                        } else {
-                            self.offset = .zero
-                        }
-                    }
-        
-        
-        return bodyView()
-            
-            
-      
-}
+    @State private var isDragging = false
+    @State private var position=CGSize.zero
     
-    struct bodyView :View{
-        var body: some View{
-            ZStack{
-                Image("spaceAwkward")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
+    
+    var body: some View{
+        ZStack{
+            Image("spaceAwkward")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
                 
-                Image("monsterName")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 265)
-                    .offset(x:155, y:-110)
+            Image("monsterName")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 265)
+                .offset(x:155, y:-110)
                     
                 
-                Image("roseMonster")
+            Image("roseMonster")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            
+            ZStack{
+                Image("leapRight")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                
-                //leapView()
-                   
-            }
+                    .rotationEffect(.degrees(Double(position.width / 5)))
+                    .offset(x: position.width * 5, y: 0)
+                    .opacity(2 - Double(abs(position.width / 50)))
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                position = value.translation
+                                isDragging = true
+                            }
+                            .onEnded{ _ in
+                                if abs(position.width) > 100 {
+                                    removal?()
+                                } else {
+                                    position = .zero
+                                }
+                                isDragging = false
+                            }
+                    )
             }
         }
     }
-
-/*
-struct leapView : View{
-    
-    var body : some View{
-        
-       let right = Image("leapRight")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-
-       let left =  Image("leapLeft")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-    }
-    
 }
- */
-
-
-
-/*
-struct Stage1_Main: View {
-    var body: some View {
-        Text("리나 시즈 화이팅")
-    }
-}
-
-struct Stage1_Main_Previews: PreviewProvider {
-    static var previews: some View {
-        Stage1_Main()
-    }
-}
-*/
