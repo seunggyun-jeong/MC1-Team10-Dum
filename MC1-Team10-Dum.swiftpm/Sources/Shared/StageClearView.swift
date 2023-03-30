8c8c3c5ba06beb092a3692dfee0433574c29e7f0
seunggyun-jeong <jskyun98@gmail.com>
2023-03-30 09:42:27 +0900

    [FEAT] Stage Ending View Template

---
+++ MC1-Team10-Dum.swiftpm/Sources/Shared/StageClearView.swift
@@ -0,0 +1,70 @@
+//
+//  StageClearView.swift
+//  
+//
+//  Created by 정승균 on 2023/03/30.
+//
+
+import SwiftUI
+
+struct StageClearView: View {
+    @Binding var deadFlag: Bool
+    @State var mentorImageName: String
+    @State var mentorName: String
+    @State var mentorSpeak: String
+    
+    var body: some View {
+        // 퀘스트 클리어 시 아웃트로
+        ZStack {
+            Rectangle()
+                .foregroundColor(.black)
+            
+            VStack {
+                Text("STAGE CLEAR")
+                    .font(.system(size: 48))
+                    .fontWeight(.bold)
+                    .foregroundColor(.yellow)
+                
+                HStack(spacing: 30) {
+                    VStack {
+                        Image(mentorImageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200)
+                        Text("Mentor. \(mentorName)")
+                            .foregroundColor(.white)
+                            .font(.system(size: 30))
+                            .bold()
+                    }
+                        Text(mentorSpeak)
+                            .foregroundColor(.white)
+                            .font(.system(size: 30))
+                            .bold()
+
+                }
+            }
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    NavigationLink(destination: OutroView()) {
+                        Image("nextButton")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200)
+                    }
+                    .disabled(!deadFlag)
+
+                }
+            }
+            .padding([.bottom, .trailing], 50)
+        }
+    }
+}
+
+struct StageClearView_Previews: PreviewProvider {
+    static var previews: some View {
+        StageClearView(deadFlag: .constant(false), mentorImageName: "GQ_monster", mentorName: "JUDY", mentorSpeak: "어색함 괴물을 물리쳤군! 수고했어!\nCBL을 위해서 가장 필요한 스킬인 소통 능력이\n 잘 장착되었길 바란다!")
+    }
+}
