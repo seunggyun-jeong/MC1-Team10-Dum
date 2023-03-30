1bbc91f8db87d37f04fcaedb8019be9166c1dd41
seunghoria <seungeun991114@gmail.com>
2023-03-30 10:34:36 +0900

    feat: stage clear 구현

--- MC1-Team10-Dum.swiftpm/Sources/Lina&Seez/Stage1_Main.swift
+++ MC1-Team10-Dum.swiftpm/Sources/Lina&Seez/Stage1_Main.swift
@@ -6,7 +6,7 @@ import SwiftUI
 struct Stage1_Main: View {
     
     @StateObject var leafVM:LeafViewModel = LeafViewModel()
-
+    @State private var deadFlag: Bool = false
     
     var body: some View{
         
@@ -36,7 +36,7 @@ struct Stage1_Main: View {
                 .resizable()
                 .scaledToFit()
                 .offset(x: -200, y:-140)
-                .animation(.linear.repeatForever().speed(5), value: leafVM.countLeaf)
+                .opacity( leafVM.countLeaf <= 2  ? 0.8 : 0.0)
             
             GeometryReader { proxy in
                 ForEach(0...4, id:\.self) { index in
@@ -45,12 +45,9 @@ struct Stage1_Main: View {
                 }
             }.animation(.easeInOut(duration: 1))
             
-            
-        ZStack{
-            Image("stage1Dark")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-            }.opacity(leafVM.countLeaf == 0 ? 0.8 : 0.0)
+           
+            StageClearView(deadFlag: $deadFlag, mentorImageName: "judyMonster", mentorName: "JUDY", mentorSpeak: "어색함 괴물을 물리쳤군! 수고했어!\nCBL에서 필수 스킬인 소통 능력 획득으로\n원팀이 되었군!")
+            .opacity(leafVM.countLeaf == 0  ? 0.8 : 0.0)
         }
         
         
