62de324b56f9cc479cce68b62857f16b5d5a99e4
HansolWorld <hs9269@gmail.com>
2023-03-30 09:29:46 +0900

    change ontab animation

--- MC1-Team10-Dum.swiftpm/Sources/Sol&Cindy/Stage2_Main.swift
+++ MC1-Team10-Dum.swiftpm/Sources/Sol&Cindy/Stage2_Main.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct Stage2_Main: View {
     @State private var press = false
     @State private var isAnimating = false
+    @State private var decibel:Double = 0
+    @State private var monsterCount: Int = 5
+    
+    @State private var timer: Timer?
+    @ObservedObject var recorder = Recorder()
     
     var body: some View {
         ZStack {
@@ -30,21 +35,20 @@ struct Stage2_Main: View {
                     .scaledToFit()
                     .frame(width:929)
                     .offset(x: self.isAnimating ? -10 : 10, y: self.isAnimating ? -10 : 10)
-                    .animation(Animation.easeInOut(duration:0.3)
-                        .repeatForever(autoreverses: true))
-                        .onAppear {
-                                    self.isAnimating = true
-                                    }
+                    .animation(Animation.easeInOut(duration:0.3).repeatForever(autoreverses: true), value: isAnimating)
+
                 Image("stage2_say")
                     .resizable()
                     .scaledToFit()
                     .frame(width:300)
                     .offset(x:-400,y:-160)
-                Text(press ? "누르고 있습니다" : "누르고 있지 않습니다")
                 Image("stage2_backdum")
                     .resizable()
                     .scaledToFit()
                     .offset(x:0,y:220)
+                
+                Text(press ? "누르고 있습니다 \(monsterCount) \(decibel)" : "누르고 있지 않습니다 \(monsterCount) \(decibel)")
+                
                 Button(action:{}) {
                     Image("stage2_button")
                 }
@@ -52,10 +56,23 @@ struct Stage2_Main: View {
                 .simultaneousGesture(
                     DragGesture(minimumDistance: 0)
                         .onChanged({ _ in
-                            self.press = true
+                            recorder.startRecording()
+                            self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+                                self.decibel = recorder.updateDecibels()
+                                
+                                if self.decibel > 100 {
+                                    isAnimating = true
+                                }
+                                
+                                if self.decibel > 150 {
+                                    monsterCount -= 1
+                                    timer?.invalidate()
+                                    recorder.stopRecording()
+                                }
+                            }
                         })
-                        .onEnded({ _ in
-                            self.press = false
+                        .onEnded( { _ in
+                            isAnimating = false
                         })
                 )
             }
